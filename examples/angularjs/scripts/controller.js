1777c1d575d8a12e91a93af53574df8731b05c86
FEvelychko <gitjsdeveloper@gmail.com>
2015-05-25 00:47:00 +0300

    total functional on angularjs

--- examples/angularjs/scripts/controller.js
+++ examples/angularjs/scripts/controller.js
@@ -3,19 +3,40 @@
  */
     "use strict";
 
-var tableApp = angular.module('tableApp',[]);
-tableApp.controller('tableController', ['$scope', function($scope) {
+    var tableApp = angular.module('tableApp',[]);
+    tableApp.controller('tableController', ['$scope', function($scope) {
 
-    $scope.headers = ["id", "name", "stock", "country"];
-    $scope.data = [
-        {id:1, name: "apple", stock:125, country: "USA"},
-        {id:2, name: "facebook", stock: 78, country: "USA"},
-        {id:3, name: "microsoft", stock: 559, country: "USA"},
-        {id:4, name: "softServe", stock: 100, country: "Ukraine"},
-        {id:5, name: "luxSoft", stock: 90, country: "Ukraine"},
-        {id:6, name: "siemens", stock: 59, country: "Germany"},
-        {id:7, name: "globalLogic", stock: 30, country: "Ukraine"},
-        {id:8, name: "twitter", stock: 50, country: "USA"}
-    ];
+        $scope.data = [];
+        $scope.rowTableObj = {};
+        $scope.rowObject = {};
+        $scope.currentObj = {};
+        $scope.headers = ["id", "name", "country", "product", "founder", "stock", "change data"];
+        $scope.realData = [
+            {id:1, name: "apple", country: "USA", product: "iPad", founder:"Jhon", stock:100},
+            {id:2, name: "facebook", country: "USA", product: "social Web Site", founder:"Max", stock:200},
+            {id:3, name: "microsoft", country: "USA", product: "PC", founder:"Kiril", stock:300},
+            {id:4, name: "softServe", country: "Ukraine", product: "software", founder:"Andrew", stock:40},
+            {id:5, name: "luxSoft", country: "Ukraine", product: "software", founder:"Anton", stock:30},
+            {id:6, name: "siemens", country: "Germany", product: "mobiles", founder:"Jason", stock:110},
+            {id:7, name: "globalLogic", country: "Ukraine", product: "software", founder:"Wesney", stock:40}
+        ];
+
+        $scope.addTable = function(){
+            $scope.showTable = true;
+        };
+
+        $scope.saveData = function(){
+            $scope.rowObject = $scope.currentObj;
+            $scope.data.push($scope.rowObject);
+            $scope.currentObj = {};
+        };
+        $scope.deleteData = function(index){
+                $scope.data.splice(index, 1);
+        };
+
+        $scope.changeData = function(){
+                $scope.currentObj = $scope.rowObject;
+                $scope.data.pop($scope.rowObject);
+        }
 
 }]);
